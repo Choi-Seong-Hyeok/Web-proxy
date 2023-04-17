6aecebad0f2d1d6d472d5763d784f7ee6e453306
Ubuntu <ubuntu@ip-172-31-32-140.ap-northeast-2.compute.internal>
2023-04-17 12:09:41 +0000

    tiny web 수정

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -35,36 +35,44 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
 // time_t st_ctime; /* time of last status change */ 
 // }
 
-// 응답을 해주는 함수
-void doit(int fd)
+// 한개의 HTTP 트랜잭션을 처리함.
+void doit(int fd) // fd = file descriptor = 파일 식별자
 {
   // 정적파일인지 아닌지를 판단해주기 위한 변수
   int is_static;
   // 파일에 대한 정보를 가지고 있는 구조체
   struct stat sbuf;
+  // HTTP request를 받아들이기 위한 버퍼 및 각각의 요청 메서드, URI, HTTP version을 저장
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  // 각각 static / dynamic content의 파일명 및 CGI인자들을 저장.
+  // 'filenam'은 HTTP request에서 추출한 파일의 이름을 저장
+  // 'cgiargs'는 HTTP request에서 추출한 CGI 인자들을 저장
   char filename[MAXLINE], cgiargs[MAXLINE];
+  // rio 라이브러리를 사용하기 위한 구조체. rio 라이브러리는 표준 C 라이브러리의 I/O함수를 래핑하여, 더욱 안정적인 I/O를 제공.
   rio_t rio;
 
   /* Read request line and headers */
   // rio(robust I/O (Rio)) 초기화
   Rio_readinitb(&rio, fd);
   // buf에서 client request 읽어들이기
+  // rio_readlineb함수를 사용해서 요청 라인을 읽어들임
   Rio_readlineb(&rio, buf, MAXLINE);
   printf("Request headers:\n");
   // request header 출력
   printf("%s", buf);
   // buf에 있는 데이터를 method, uri, version에 담기
   sscanf(buf, "%s %s %s", method, uri, version);
-  // method가 GET이 아니라면 error message 출력
-  // problem 11.11을 위해 HEAD 추가
+
+  // method가 GET이 아니라면 error message 출력 + main 루틴으로 회귀
+  // 그 후에 연결을 닫고 다음 요청을 기다림
   if (strcasecmp(method, "GET")) {
     clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
     return;
   }
+  // 다른 요청 헤더들을 무시한다.
   read_requesthdrs(&rio);
 
-  /* Parse URI from GET request */
+  /* URI를 파일 이름과 비어 있을 수도 있는 CGI 인자 스트링으로 분석하고, 요청이 정적 또는 동적 컨텐츠를 위한 것인지 나타내는 플래그 */
   is_static = parse_uri(uri, filename, cgiargs);
   // filename에 맞는 정보 조회를 하지 못했으면 error message 출력 (return 0 if success else -1)
   if (stat(filename, &sbuf) < 0) {
@@ -74,11 +82,11 @@ void doit(int fd)
   // request file이 static contents이면 실행
   if (is_static) {
     // file이 정규파일이 아니거나 사용자 읽기가 안되면 error message 출력
-    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {// 이 파일이 보통 파일 이라는 것과 읽기 권한을 가지고 있는지를 검증
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
       return;
     }
-    // response static file
+    // 위 검증에서 정적파일이라는게 정적파일을 클라이언트에게 제공
     serve_static(fd, filename, sbuf.st_size, method);
   }
   // request file이 dynamic contents이면 실행
@@ -89,7 +97,7 @@ void doit(int fd)
       "Tiny couldn't run the CGI program");
       return;
   }
-  // response dynamic files
+  // 위 검증을 거치면 동적 컨텐츠를 클라이언트에게 제공
     serve_dynamic(fd, filename, cgiargs, method);
   }
 }
@@ -99,6 +107,7 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
 {
   char buf[MAXLINE], body[MAXBUF];
   // response body 쓰기 (HTML 형식)
+  // 브라우저 사용자에게 에러를 설명하는 응답 본체에 HTML 파일도 함께 보냄
   sprintf(body, "<html><title>Tiny Error</title>");
   sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
   sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
@@ -262,7 +271,7 @@ void serve_dynamic(int fd, char *filename, char *cgiargs, char *method)
 }
 
 
-//argc(argument count): 인자 개수(올바른 인자개수는 2개 --> 실행할 프로그램 , 포트번호) , argv: par 값
+// argc(argument count): 인자 개수(올바른 인자개수는 2개 --> 실행할 프로그램 , 포트번호) , argv: par 값
 // Ex ) ./tiny 8080 면 argc는 2 , argv는 tiny와 8080 두 개 들어감
 int main(int argc, char **argv)             
 {
@@ -271,30 +280,40 @@ int main(int argc, char **argv)
     argv[1] : 포트번호
   */
 
+  // listenfd : 들어오는 연결 요청을 받기 위한 소켓 식별자
+  // connfd : 연결된 클라이언트 소켓 식별자
   int listenfd, connfd;
+  // hostname : 'getnameinfo' 함수에서 클라이언트 소켓의 IP주소를 저장하기 위한 문자열 변수.
+  // port : 'getnameinfo' 함수에서 클라이언트 소켓의 포트 번호를 저장하기 위한 문자열 변수.
   char hostname[MAXLINE], port[MAXLINE];
-  // client socket's length
+ 
+  // accept로 보내지는 소켓 주소 구조체의 크기정보
   socklen_t clientlen;
-  // client socket's addr
+  // accept로 보내지는 소켓 주소 구조체
   struct sockaddr_storage clientaddr;
 
-  // port number를 입력안했을 경우 error
+  // 입력 인자가 2개가 아니면
   if (argc != 2) {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(1);
   }
 
-  //arg로 받은 port number로 listen socket으로 변경
-  // port 받은거 넣음
+  // 듣기 소켓 오픈 ! 
   listenfd = open_listenfd(argv[1]);
   // 무한 서버 루프 실행
   while (1) {
     clientlen = sizeof(clientaddr);
+    /*
+      서버는 accept 함수를 호출해서 클라이언트로의 연결 요청을 기다린다.
+      client 소켓은 server 소켓의 주소를 알고 있으니까
+      client에서 server로 넘어올 때 add정보를 가지고 올 것이라고 가정
+      accept 함수는 연결되면 식별자 connfd를 return.
+    */
     connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);                       // SA: type of struct socketaddr
     // client socket에서 hostname과 port number를 스트링으로 변환
     getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);
-    close(connfd);
+    doit(connfd); // 트랜잭션 수행 즉, 클라이언트가 요청한 작업을 수행하고, 클라이언트에게 응답을 보내는 작업을 수행합니다.
+    close(connfd); // 클라이언트와의 연결을 종료하는 작업을 수행
   }
 }
